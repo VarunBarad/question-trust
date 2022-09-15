801461183cc0967ab89ccc77f68fc5a95329d956
Varun Barad <varun@varunbarad.com>
2022-09-15 16:40:49 +0530

    Add service-worker and make it PWA

---
+++ service-worker.js
@@ -0,0 +1,24 @@
+const cacheName = 'trust-invitation';
+const filesToCache = [
+    '/',
+    '/index.html',
+    '/index.css',
+    '/index.js',
+];
+
+self.addEventListener('install', function (event) {
+    const promiseChain = Promise.resolve()
+        .then(() => caches.delete(cacheName))
+        .then(() => caches.open(cacheName))
+        .then((cache) => cache.addAll(filesToCache));
+
+    event.waitUntil(promiseChain);
+});
+
+self.addEventListener('fetch', function (event) {
+    event.respondWith(
+        caches.match(event.request).then(function (response) {
+            return response || fetch(event.request);
+        })
+    );
+});
